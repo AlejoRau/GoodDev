a789dc38924b3efda426065feb20441b6ea1ce75
Alejo Rau <105177309+AlejoRau@users.noreply.github.com>
2025-11-11 17:03:06 -0300

    workflow

--- code.js
+++ code.js
@@ -42,6 +42,11 @@ class UserController {
     this.users = this.users.filter(u => u.id != req.params.id);
     res.send("Usuario eliminado."); 
   }
+
+  deletePerson(req, res) {
+    this.users = this.users.filter(u => u.id != req.params.id);
+    res.send("Usuario eliminado."); 
+  }
 }
 
 const controller = new UserController();
